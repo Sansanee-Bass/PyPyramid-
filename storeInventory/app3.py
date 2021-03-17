4678b4f25487fe65550a3f4cb6517a959df2310d
Christopher <drakochris1992@gmail.com>
2021-03-17 01:08:56 -0400

    been workin for hours almost done need to work on nav and db conn folders left

--- storeInventory/app3.py
+++ storeInventory/app3.py
@@ -7,14 +7,14 @@ import mysql.connector  # imported sql connector for database
 
 #----------------- Show  page Home --------------------------#
 @view_config(
-    route_name='hello',
+    route_name='home',
     renderer='view/templates/home.jinja2'
 )
 def home(request):
     conn = mysql.connector.connect(
         host="localhost",
         user="web_user",
-        passwd='0eTnBQz4yE3jfO6R',  # please enter pws 0eTnBQz4yE3jfO6R
+        passwd='4aOMcvLHen7Cepxo',  # please enter pws 0eTnBQz4yE3jfO6R
         database="pyramidproject")
     cur = conn.cursor()
     cur.execute('SELECT Id, name, total, link FROM Inventory')
@@ -24,14 +24,16 @@ def home(request):
     return{"foot": 'SanChris thanks you for trusting our products', "greeting": 'Welcome to SanChris Inventory Market', "inv": inv}
 
 
-#----------------- Show page edit ---------------------#
-@view_config(
+#----------------- Show page edit/id ---------------------#
+@ view_config(
     route_name='edit',
     renderer='view/templates/edit.jinja2'
+
+
 )
 def edit(request):
     conn = mysql.connector.connect(
-        host="localhost", user="web_user", passwd='0eTnBQz4yE3jfO6R', database="pyramidproject")  # please enter ypur pws
+        host="localhost", user="web_user", passwd='4aOMcvLHen7Cepxo', database="pyramidproject")  # please enter ypur pws
     cur = conn.cursor()
 
     if request.method == 'POST':
@@ -46,19 +48,58 @@ def edit(request):
         cur.execute(
             "SELECT Id, name, total, link FROM Inventory WHERE Id = %(id)s", {'id': request.matchdict['id']})
         (id, name, total, link) = cur.fetchone()
-        return {"foot": 'SanChris thanks you for trusting our products', "greeting": 'Edit Stock Inventory', "name": '', 'item': {'id': id, 'name': name, 'total': total, 'link': link}}
+        return {"foot": 'SanChris thanks you for trusting our products', "greeting": 'Edit Inventory', "name": '', 'item': {'id': id, 'name': name, 'total': total, 'link': link}}
 
 
-#------------- Set up and run pyramid ang jinja2 --------------------#
+#--------------------inventory page-------------------------------------------------#
+@ view_config(
+    route_name='inventory',
+    renderer='view/templates/inventory.jinja2'
+)
+def inventory(request):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="web_user",
+        passwd='4aOMcvLHen7Cepxo',  # please enter pws 0eTnBQz4yE3jfO6R
+        database="pyramidproject")
+    cur = conn.cursor()
+    cur.execute('SELECT Id, name, total, link FROM Inventory')
+    inv = []
+    for (id, name, total, link) in cur:
+        inv.append({'id': id, 'name': name, 'total': total, 'link': link})
+    return{"foot": 'SanChris thanks you for trusting our products', "greeting": 'Please take your time and browse of site', "inv": inv}
+#--------------------product page-------------------------------------------------#
+
+
+@ view_config(
+    route_name='product',
+    renderer='view/templates/product.jinja2'
+)
+def product(request):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="web_user",
+        passwd='4aOMcvLHen7Cepxo',  # please enter pws 0eTnBQz4yE3jfO6R
+        database="pyramidproject")
+    cur = conn.cursor()
+    cur.execute('SELECT Id, name, total, link FROM Inventory')
+    inv = []
+    for (id, name, total, link) in cur:
+        inv.append({'id': id, 'name': name, 'total': total, 'link': link})
+    return{"foot": 'SanChris thanks you for trusting our products', "greeting": 'Please take your time and browse of site', "inv": inv}
+
+
+#------------- Set up and run pyramid and jinja2 --------------------#
 if __name__ == "__main__":
     config = Configurator()
     config.include('pyramid_jinja2')
     config.include('pyramid_debugtoolbar')
     config.add_static_view(name='static',
                            path='model/static')
-    config.add_route('hello', '/')
+    config.add_route('home', '/')
     config.add_route('edit', '/edit/{id}')
-
+    config.add_route('inventory', '/inventory')
+    config.add_route('product', '/product')
     config.scan()
     app = config.make_wsgi_app()
 server = make_server('0.0.0.0', 8080, app)  # default port for pyramid is 6543
--- storeInventory/pyramidproject.sql
+++ storeInventory/pyramidproject.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               10.4.14-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.5919
+-- HeidiSQL Version:             11.1.0.6116
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,6 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Dumping database structure for pyramidproject
@@ -30,14 +31,34 @@ CREATE TABLE IF NOT EXISTS `inventory` (
 -- Dumping data for table pyramidproject.inventory: ~6 rows (approximately)
 /*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
 REPLACE INTO `inventory` (`Id`, `name`, `total`, `link`) VALUES
-	(1, 'Dress', 3, '\\static\\dress.jpg'),
-	(2, 'Jean', 1, '\\static\\jean.jpg'),
-	(3, 'Short', 0, '\\static\\shorts.jpg'),
-	(4, 'Skirt', 0, '\\static\\skirt.jpeg'),
-	(5, 'Wool coat', 0, '\\static\\coat.jpeg'),
-	(6, 'Heels', 0, '\\static\\heels.jpg');
+	(1, 'Dress', 3, '/static/img/dress.jpg'),
+	(2, 'Jean', 2, '/static/img/jean.jpg'),
+	(3, 'Short', 0, '/static/img/shorts.jpg'),
+	(4, 'Skirt', 5, '/static/img/skirt.jpeg'),
+	(5, 'Coat', 0, '/static/img/coat.jpeg'),
+	(6, 'Heels', 0, '/static/img/heels.jpg');
 /*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+
+-- Dumping structure for table pyramidproject.menu
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE IF NOT EXISTS `menu` (
+  `menuId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `link` varchar(50) NOT NULL,
+  `dateModified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`menuId`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table pyramidproject.menu: ~3 rows (approximately)
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+REPLACE INTO `menu` (`menuId`, `name`, `link`, `dateModified`) VALUES
+	(1, 'Home', '/', '2021-03-17 00:25:11'),
+	(2, 'Products', '/product', '2021-03-17 00:25:16'),
+	(3, 'Inventory', '/inventory', '2021-03-17 00:25:33');
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
